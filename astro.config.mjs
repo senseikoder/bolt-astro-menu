02eba423add25f3509a73f4613a605e18e20e685
Frank Albarracín <molina.frank@gmail.com>
2024-10-17 19:44:46 -0400

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   vite: {
     ssr: {
       noExternal: ['bulma'],
     },
   },
+
   output: "hybrid",
   integrations: [],
+  adapter: netlify(),
 });
